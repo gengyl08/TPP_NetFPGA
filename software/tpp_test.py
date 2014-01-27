caaeb350cc06d6b083830f8cec6f717726d50688
Yilong Geng <gengyl08@stanford.edu>
2014-01-26 17:33:39 -0800

    better sw

--- software/tpp_test.py
+++ software/tpp_test.py
@@ -1,5 +1,17 @@
 from scapy import *
 from scapy.all import *
 
-data = 'A'*64 + chr(0)+chr(3)+chr(0)*100
+LOAD_REG_HOPMEM = 0
+STORE_HOPMEM_REG = 2
+
+padding_head = 'A'*64
+
+instruction = chr(LOAD_REG_HOPMEM)
+
+reg_addr = chr(2)
+
+padding_tail = chr(0)*100
+
+data = padding_head + instruction + reg_addr + padding_tail
+
 sendp(Ether(data), iface='nf3')
